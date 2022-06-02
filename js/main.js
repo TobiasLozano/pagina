fe2eb2d2f95632cbdcdd2759680537cf3e04896d
Tobias Lozano <88689090+TobiasLozano@users.noreply.github.com>
2022-06-02 18:23:02 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* const enviar = document.getElementById('send');
+enviar.onclick=()=>{
+    const correo = document.getElementById('mail').value;
+    alert(correo);
+    const formulario= document.getElementById('form');
+    const send = `https://formsubmit.co/${correo}`;
+    formulario.setAttribute('action', send)
+}
+
+ */
